a6788a02f9db92c7aa59ea88d26dcd508bd59d92
Egor <111316323+Voppor41@users.noreply.github.com>
2024-07-20 00:28:41 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,6 +1,6 @@
 count_of_homeworks = 12
 spent_hours = 1.5
 name_of_course = "Python"
-time_for_one_task = count_of_homeworks/spent_hours
+time_for_one_task = spent_hours/count_of_homeworks
 
 print(f"Курс: {name_of_course}, всего задач: {count_of_homeworks}, затрачено часов: {spent_hours}, среднее время выполнения {time_for_one_task} часа")
