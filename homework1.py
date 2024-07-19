17ca05d70aba2c3ef5702361a5f08cfc6f5ec385
Egor <111316323+Voppor41@users.noreply.github.com>
2024-07-19 20:03:13 +0300

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -4,9 +4,4 @@ print(example[0])
 print(example[-1])
 print(example[5::])
 print(example[::-1])
-
-for i in range(len(example)):
-    if i != 0 and i % 2 != 0:
-        b += example[i]
-
-print(b)
+print(example[1:10:2])
