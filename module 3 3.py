0379c7104d01c7c1fbe764f41afac625a69708b1
Egor <111316323+Voppor41@users.noreply.github.com>
2024-08-23 13:47:25 +0300

    Update module 3 3.py

--- module 3 3.py
+++ module 3 3.py
@@ -4,9 +4,9 @@ def print_params(a = 1, b = 'строка', c = True):
 print_params(b = 25)
 print_params(c = [1, 2, 4])
 values_list = [4, 'hello', False]
-values_dict = {"1": "asd", "2": False, "3": 52}
-print_params(*values_dict)
+values_dict = {"a": "asd", "b": False, "c": 52}
+print_params(*values_list)
 print_params(**values_dict)
-values_list2 = [3, 'io']
-print_params(*values_list2)
+values_list_2 = [3, 'io']
+print_params(*values_list_2, 42)
 
